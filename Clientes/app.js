09097a0159f25fd3a038f75c988af38511da81a8
DvB97 <danielantomy@gmail.com>
2021-03-29 16:17:35 -0500

    daniel
    xxdxd

--- Clientes/app.js
+++ Clientes/app.js
@@ -1,26 +1,34 @@
 $(function(){
-    $.ajax({
-        url: 'listar.php',
-        type: 'GET',
-        success: function(respuesta){
-            let contador = 0;
-            let r="";
-            let lista = JSON.parse(respuesta);
-            lista.forEach(element => {
-                r +="<tr>"+
-                        "<td></td>"+
-                        "<td></td>"+ 
-                        "<td>"+element.id+"</td>"+
-                        "<td>"+element.nombre+"</td>"+
-                        "<td>"+element.empresa+"</td>"+
-                    "</tr>";
-                contador++;
-            });
+    
 
-            //llenar datos html
-            $('#listaCLiente').html(r);
-            $('#cantidadCliente').html("Actualmente hay un total de "+contador+" Clientes");
-        }   
+    $('#buscar').keyup(function(){
+        if($('#buscar').val()){
+            let contador = 0;
+            let buscado = $('#buscar').val();
+            $.ajax({
+                url: 'buscar.php',
+                type: 'POST',
+                data: {buscado},
+                success: function(respuesta){
+                    let tarea = JSON.parse(respuesta);
+                    let r ="";
+                    tarea.forEach(element => {
+                        r +="<tr>"+
+                                "<td></td>"+
+                                "<td></td>"+ 
+                                "<td>"+element.id+"</td>"+
+                                "<td>"+element.nombre+"</td>"+
+                                "<td>"+element.empresa+"</td>"+
+                            "</tr>"; 
+                            console.log(element.nombre);                     
+                        contador++;
+                    });
+                //llenar datos html
+                $('#listaCLiente').html(r);
+                $('#cantidadCliente').html("Actualmente hay un total de "+contador+" Clientes");
+                }
+            })
+        }
     })
 
     $('#buscar').keyup(function(){        
